06f03e093f5e0c6941fab43bd4f94eb951de0427
Vineet Sharma <vineet.sharma@digicraft.ai>
2025-12-05 10:16:56 +0530

    Dec 5

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+app.get('/health', (req, res) => {
+  res.send('OK');
+})
+
 // Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
