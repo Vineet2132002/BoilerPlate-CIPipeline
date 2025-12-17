d7404bf3fad5b0df92af842867e0910a1109e8e8
Vineet Sharma <vineet.sharma@digicraft.ai>
2025-12-17 16:49:04 +0530

    script sequence issue fixed sym link accessed before being created

--- scripts/switch-symlink.sh
+++ scripts/switch-symlink.sh
@@ -6,10 +6,12 @@ set -e
 RELEASE_DIR="/opt/ci-pipeline-demo/releases/release-${DEPLOYMENT_ID}"
 CURRENT_LINK="/opt/ci-pipeline-demo/current"
 
+ls -l /opt/ci-pipeline-demo/
+
 echo "Switching symlink to new release directory: $RELEASE_DIR"
 
 # ln -> link command
 # -s -> create a symbolic link
 # -f -> forcefully remove existing destination files
 # -n -> treat destination that is a symlink to a directory as a normal file, preventing recursive linking
-ln -sfn "$RELEASE_DIR" "$CURRENT_LINK"
+sudo ln -sfn "$RELEASE_DIR" "$CURRENT_LINK"
