22f5c919d1959a0915202c9a2192988567ff6f63
Vineet Sharma <vineet.sharma@digicraft.ai>
2025-12-18 12:11:40 +0530

    Dec 18, 2025 12:10

--- scripts/rollback.sh
+++ scripts/rollback.sh
@@ -4,4 +4,7 @@ cd $(readlink -f /opt/ci-pipeline-demo/current)
 docker compose down
 ln -sfn /opt/ci-pipeline-demo/releases/$RELEASE /opt/ci-pipeline-demo/current
 cd $(readlink -f /opt/ci-pipeline-demo/current)
-sudo docker compose up -d
+
+# Load the Docker image for specific version
+docker load -i ci-pipeline-demo.tar
+docker compose up -d
